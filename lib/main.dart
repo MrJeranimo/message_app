5c99ab002806d698bf816d4d02b1a44a50e921f5
Jeremy Harrington <harringtonj6@xavier.edu>
2025-08-14 16:47:51 -0500

    Added an AppBar and attempting to create a uniform bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,61 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 0, 0),
-        body: Center(
-          child: Text('Hello !'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Message App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
+        home: MyHomePage(),
       ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      appBar: MyAppBar(title='Home'),
+      body: Column(
+        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends AppBar {
+  Widget build(BuildContext context, String title) {
+    var homeIcon = Icon(Icons.home);
+    
+    return AppBar(
+      backgroundColor: Colors.blue[700],
+      title: Text(title),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: print("Hello"),
+          icon: homeIcon,
+          ),
+      ],
     );
   }
 }
